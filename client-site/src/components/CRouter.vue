3daa9f78e0bd0e7e51e6e22e308205fa164dc04b
Kukonen <ekukonen@gmail.com>
2023-09-02 21:07:36 +0300

    fix:

--- client-site/src/components/CRouter.vue
+++ client-site/src/components/CRouter.vue
@@ -4,12 +4,20 @@
 
 <template>
     <router-view v-slot="{ Component, route }">
-        <transition name="fade">
+        <transition name="fade" mode="out-in">
             <component :is="Component" :key="route.path" />
         </transition>
     </router-view>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.25s ease-in-out;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
 </style>
