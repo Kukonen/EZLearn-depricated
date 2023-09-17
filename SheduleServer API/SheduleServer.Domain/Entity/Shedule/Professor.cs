adfc34c0810d350ac497c37ffa86d611a99d8ddf
Kukonen <ekukonen@gmail.com>
2023-09-17 18:35:05 +0300

    feat: sever api init

---
+++ SheduleServer API/SheduleServer.Domain/Entity/Shedule/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheduleServer.Domain.Entity.Shedule
+{
+	public class Professor
+	{
+        public decimal Id { get; set; }
+        public string Name { get; set; }
+
+		public virtual IEnumerable<SheduleLessonProfessor> SheduleLessonProfessors { get; set; }
+	}
+}
---
+++ SheduleServer API/SheduleServer.Domain/Entity/Shedule/SheduleLessonProfessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheduleServer.Domain.Entity.Shedule
+{
+	public class SheduleLessonProfessor
+	{
+		public int Id { get; set; }
+
+        public int SheduleLessonId { get; set; }
+        public SheduleLesson SheduleLesson { get; set; }
+
+        public int ProfessorId { get; set; }
+        public Professor Professor { get; set; }
+    }
+}
